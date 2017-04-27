6b308fd4b722f761ffd46cd4490b9543bd52af61
Perk430 <perk430@gmail.com>
2017-04-27 16:38:20 -0400

    fix merge

--- Midterm/Program.cs
+++ Midterm/Program.cs
@@ -26,13 +26,21 @@ namespace Midterm
 
             do {
 
-                HiddenMinefield minefield = new HiddenMinefield(rows, columns, bombs);
+                HiddenMinefield minefield1 = new HiddenMinefield(rows, columns, bombs);
 
+                HiddenMinefield.AddBombs(minefield1.Minefield, 10);
+
+                Console.WriteLine(minefield1.Minefield);
+
+
+                VisibleMinefield minefield2 = new VisibleMinefield(minefield1.Minefield);
+                VisibleMinefield.PrintHiddenArray(minefield2.VisMinefield);
                 //Initiate game (Print, get choice, check & update array, repeat)
 
                 //Continue?
                 //Console.WriteLine("{0}", (char)224);  printing ascii characters
                 //VisibleMinefield.PrintHiddenArray();
+
             }while(Validation.Continue());
         }
     }
