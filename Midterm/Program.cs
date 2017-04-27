bb9842c79ae093447996aef576d86d073d60f721
Perk430 <perk430@gmail.com>
2017-04-27 13:51:23 -0400

    Added information for rows and columns to main

--- Midterm/Program.cs
+++ Midterm/Program.cs
@@ -11,12 +11,21 @@ namespace Midterm
         static void Main(string[] args)
         {
             //get user input for minefield size and # of bombs(pass to Minefield constructor)
+            Console.WriteLine("Welcome to the Minefield! \n");
+            Console.WriteLine("Please enter the size of your minefield: (x, y) ");
+            Console.Write("Rows: ");
+            int rows = Validation.GetNumberInRange(2, 20);
+
+            Console.Write("Columns: ");
+            int columns = Validation.GetNumberInRange(2, 20);
+
+
 
             //Initiate game (Print, get choice, check & update array, repeat)
 
             //Continue?
             //Console.WriteLine("{0}", (char)224);  printing ascii characters
-            VisibleMinefield.PrintHiddenArray();
+            //VisibleMinefield.PrintHiddenArray();
         }
     }
 }
