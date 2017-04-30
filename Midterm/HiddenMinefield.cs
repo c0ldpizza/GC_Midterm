a2d291c89e391b4cb7c4704cf709e21aeb012fdc
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-29 20:15:36 -0400

    Changed how adjacent bombs are calculated. Still buggy

--- Midterm/HiddenMinefield.cs
+++ Midterm/HiddenMinefield.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Midterm
 {
-    class HiddenMinefield: Minefield
+    class HiddenMinefield : Minefield
     {
         private int[,] minefield;
-        
+
         public int[,] Minefield
         {
             get
@@ -34,10 +34,23 @@ namespace Midterm
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    minefield[i, j] = -3;
+                    minefield[i, j] = 0; //do this for every cell that is >= -1
                 }
             }
-            AddBombs(minefield, bombs);           
+
+            AddBombs(minefield, bombs);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (minefield[i, j] == -1)
+                    {
+                        //BombCountSimple(i, j, minefield);
+                        AddNumbers(minefield, i, j);
+                    }
+                }
+            }
         }
 
         //Add bombs to hidden array, makes sure two bombs aren't place on same space
@@ -46,8 +59,8 @@ namespace Midterm
             Random rnd = new Random();
             for (int i = 0; i < numBombs; i++)
             {
-                int r = rnd.Next(1, minefield.GetLength(0));
-                int c = rnd.Next(1, minefield.GetLength(1));
+                int r = rnd.Next(0, minefield.GetLength(0));
+                int c = rnd.Next(0, minefield.GetLength(1));
 
                 if (minefield[r, c] == 0)
                 {
@@ -60,74 +73,120 @@ namespace Midterm
             }
         }
 
-        //Method that checks a single space. Also not working
-        public static void BombCountSimple(int row, int col, int[,] minefield, int[,] visMinefield)
+        public static void AddNumbers(int[,] minefield, int bombRow, int bombCol)
         {
-            int adjBombs = 0;
-
-            for (int i = row - 1; i < row + 1; i++)
+            for (int i = bombRow - 1; i < bombRow + 1; i++)
             {
-                for (int j = col - 1; j < col + 1; j++)
+                for (int j = bombCol - 1; j < bombCol + 1; j++)
                 {
-                    if (i >= 0 && i <= minefield.GetLength(0)
-                        && j >= 0 && j <= minefield.GetLength(1))
+                    if (i >= 0 && i < minefield.GetLength(0)
+                        && j >= 0 && j < minefield.GetLength(1))
                     {
-                        if (i != row || j != col)
+                        if (i != bombRow || j != bombCol)
                         {
-                            if (minefield[i, j] == -1)
+                            if (minefield[i, j] >= 0)
                             {
-                                adjBombs++;
-                            }
-                        }
-                    }
-                }
-            }
-            minefield[row, col] = adjBombs;
-            visMinefield[row, col] = adjBombs;
-            
-        }
-
-        //Recursive method to reveal number of adjacent bombs, NOT WORKING
-        public static void BombCount(int row, int col, int[,] minefield, int[,] visMinefield)
-        {
-            int adjBombs = 0;
-
-            for (int i = row - 1; i < row + 1; i++)
-            {
-                for (int j = col - 1; j < col + 1; j++)
-                {
-                    if (i >= 0 && i <= minefield.GetLength(0)
-                        && j >= 0 && j <= minefield.GetLength(1))
-                    {
-                        if (minefield[i, j] == -1)
-                        {
-                            adjBombs++;
-                        }
-                    }
-                }
-            }
-            if (adjBombs != 0)
-            {
-                visMinefield[row, col] = adjBombs;
-            }
-            else if (adjBombs == 0)
-            {
-                for (int i = row - 1; i < col + 1; i++)
-                {
-                    for (int j = col - 1; j < col + 1; j++)
-                    {
-                        if (i >= 0 && i < minefield.GetLength(0)
-                            && j >= 0 && j < minefield.GetLength(1))
-                        {
-                            if (visMinefield[i, j] == 0)     //fix
-                            {
-                                visMinefield[i, j] = 0;
-                                BombCount(i, j, minefield, visMinefield);
+                                minefield[i, j] = (minefield[i, j] + 1);
                             }
                         }
                     }
                 }
             }
         }
+
+        ////Method that checks a single space. Also not working
+        //public static void BombCountSimple(int row, int col, int[,] minefield, int[,] visMinefield)
+        //{
+        //    int adjBombs = 0;
+
+        //    for (int i = row - 1; i < row + 1; i++)
+        //    {
+        //        for (int j = col - 1; j < col + 1; j++)
+        //        {
+        //            if (i >= 0 && i <= minefield.GetLength(0)
+        //                && j >= 0 && j <= minefield.GetLength(1))
+        //            {
+        //                if (i != row || j != col)
+        //                {
+        //                    if (minefield[i, j] == -1)
+        //                    {
+        //                        adjBombs++;
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    minefield[row, col] = adjBombs;
+        //    visMinefield[row, col] = adjBombs;
+
+        //}
+
+        ////overloaded method to preset adj bombs
+        //public static void BombCountSimple(int row, int col, int[,] minefield)
+        //{
+        //    int adjBombs = 0;
+
+        //    for (int i = row - 1; i < row + 1; i++)
+        //    {
+        //        for (int j = col - 1; j < col + 1; j++)     //looping through wrong cells
+        //        {
+        //            if (i >= 0 && i <= minefield.GetLength(0)
+        //                && j >= 0 && j <= minefield.GetLength(1))
+        //            {
+        //                if (i != row || j != col)
+        //                {
+        //                    if (minefield[i, j] == -1)
+        //                    {
+        //                        adjBombs++;
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    minefield[row, col] = adjBombs;
+        //}
+
+        ////Recursive method to reveal number of adjacent bombs, NOT WORKING
+        //public static void BombCount(int row, int col, int[,] minefield, int[,] visMinefield)
+        //{
+        //    int adjBombs = 0;
+
+        //    for (int i = row - 1; i < row + 1; i++)
+        //    {
+        //        for (int j = col - 1; j < col + 1; j++)
+        //        {
+        //            if (i >= 0 && i <= minefield.GetLength(0)
+        //                && j >= 0 && j <= minefield.GetLength(1))
+        //            {
+        //                if (minefield[i, j] == -1)
+        //                {
+        //                    adjBombs++;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    if (adjBombs != 0)
+        //    {
+        //        visMinefield[row, col] = adjBombs;
+        //    }
+        //    else if (adjBombs == 0)
+        //    {
+        //        for (int i = row - 1; i < col + 1; i++)
+        //        {
+        //            for (int j = col - 1; j < col + 1; j++)
+        //            {
+        //                if (i >= 0 && i < minefield.GetLength(0)
+        //                    && j >= 0 && j < minefield.GetLength(1))
+        //                {
+        //                    if (visMinefield[i, j] == 0)     //fix
+        //                    {
+        //                        visMinefield[i, j] = 0;
+        //                        BombCount(i, j, minefield, visMinefield);
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
